3408df99bad9b60760fc7bc3fd6d93c0d94beb4a
ademhilmibozkurt <bozkurtadem7@gmail.com>
2025-12-06 15:44:36 +0300

    feature/Loading
    Loading pages and animations added to components.

---
+++ app/[[...category]]/page.js
@@ -0,0 +1,34 @@
+// [[...category]] is optinal catch all routes
+// movie/page.js will remove because prevent possible page.js render conflict
+
+import React from 'react';
+import HomeContainer from '@/containers/home';
+
+import Movies from "@/mocks/movies.json"
+
+async function delay(ms)
+{
+    return new Promise((resolve) => setTimeout(resolve,ms));
+}
+
+async function HomePage({params})
+{
+    await delay(5000);
+    let selectedCategory;
+
+    if(params.category?.length >0)
+    {
+        selectedCategory = true;
+    }
+
+    return (    
+        <HomeContainer 
+        selectedCategory={
+            {
+                id    : params.category?.[0] ?? "",
+                movies: selectedCategory ? Movies.results.slice(0,7) : [],
+            }}/>
+    );
+}
+
+export default HomePage;
--- app/movie/[id]/page.js
+++ app/movie/[id]/page.js
@@ -4,10 +4,16 @@ import {notFound} from "next/navigation";
 import MovieContainer from "@/containers/movie";
 import Movies from "@/mocks/movies.json";
 
-function MoviePage({params, searchParams})
+async function delay(ms)
 {
+    return new Promise((resolve) => setTimeout(resolve,ms));
+}
+
+async function MoviePage({params, searchParams})
+{
+    await delay(2000);
     const movieDetail = Movies.results.find(
-        (movie) => movie.id.toString() === params.id
+        (movie) => movie.id === Number(params.id)
     );
 
     if (!movieDetail)
@@ -17,7 +23,7 @@ function MoviePage({params, searchParams})
 
     if (searchParams.error === "true")
     {
-        throw new Error("Error occured!")
+        throw new Error("Error occured!");
     }
 
     return <MovieContainer movie={movieDetail}/>
